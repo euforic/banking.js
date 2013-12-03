9af16372e1c883fa71bfd4cd6b35016add0417fd
euforic <cs@euforic.co>
2013-12-02 17:25:50 -0800

    big refactor for cleaner codebase

--- test/parsing.js
+++ test/parsing.js
@@ -1,54 +1,59 @@
-var banking = require('../lib/banking')
+var Banking = require('..')
   , data = require('./fixtures/data')
   , mocha = require('mocha');
 
-describe('Ofx Statements', function(){
-  describe('getStatement', function() {
+describe('Banking', function(){
+
+  describe('banking.getStatement', function() {
     it('should return valid xml from bank server', function(done){
 
-      var bankInfo = {
-          fid: 321081669
-        , fidorg: 'DI'
-        , url: 'https://ofxdi.diginsite.com/cmr/cmr.ofx'
-        , bankid: 321081669
-        , user: 'someusername'
-        , pass: 'somepassword'
-        , accid: 12345678901
-        , acctype: 'CHECKING'
-        , date_start: 20010125
-        , date_end: 20110125
-      }
+      var banking = Banking({
+        fid: 3001,
+        fidOrg: 'Wells Fargo',
+        accType: 'checking',
+        accId: '234343434',
+        bankId: '342342',
+        user: 'username',
+        password: 'password',
+        url: 'https://www.oasis.cfree.com/3001.ofxgp'
+      });
+
 
       //If second param is omitted JSON will be returned by default
-      banking.getStatement(bankInfo, function (res, err) {
-        if(err) done(err)
-        res.should.be.an.instanceof(Object);
-        res.should.have.property('OFX');
+      banking.getStatement({start:20131101, end:20131120}, function (err, res) {
+        if(err) done(res)
+        res.body.should.be.an.instanceof(Object);
+        res.body.should.have.property('OFX');
         done();
       });
     });
   });
 
-  describe('parseOfxFile', function(){
+  describe('.version', function (){
+    it('should output the current version', function (done){
+      Banking.version.should.equal(require('../package').version);
+      done();
+    });
+  })
+
+  describe('.parseFile', function(){
     it('should read the provided file and return JSON', function(done){
-      
-      banking.parseOfxFile(__dirname +'/fixtures/sample.ofx', function (res, err) {
-        if(err) done(err)
-        res.should.be.an.instanceof(Object);
-        res.should.have.property('OFX');
-        done();  
+
+      Banking.parseFile(__dirname +'/fixtures/sample.ofx', function (res) {
+        res.body.should.be.an.instanceof(Object);
+        res.body.should.have.property('OFX');
+        done();
       });
     });
   });
 
-  describe('parseOfxString', function(){
+  describe('.parse', function(){
     it('should read the provided string and return JSON', function(done){
-      
-      banking.parseOfxString(data.ofxString, function (res, err) {
-        if(err) done(err)
-        res.should.be.an.instanceof(Object);
-        res.should.have.property('OFX');
-        done();  
+
+      Banking.parse(data.ofxString, function (res) {
+        res.body.should.be.an.instanceof(Object);
+        res.body.should.have.property('OFX');
+        done();
       });
     });
   });
